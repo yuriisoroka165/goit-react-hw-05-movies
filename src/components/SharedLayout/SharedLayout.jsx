d1fcf7429d232c31fe9e9b99b542ab930f3bf826
Yurii Soroka <yurisoroka165@gmail.com>
2023-03-21 23:18:28 +0200

    SharedLayout recreate styles with styledcomponents

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,24 +1,24 @@
 import { Suspense } from "react";
-import { Outlet, Link } from "react-router-dom";
-import css from "./SharedLayout.module.css";
+import { Outlet } from "react-router-dom";
+import { Container, Header, Link } from "./SharedLayout.styled";
 
 export function SharedLayout() {
     return (
         <>
-            <header className={css.sharedlayout__header}>
-                <nav className={css.sharedlayout__header_nav}>
+            <Header>
+                <nav>
                     <Link to="/">Home</Link>
                     <Link to="/movies">Movies</Link>
                 </nav>
-            </header>
-            <div className={"container"}>
+            </Header>
+            <Container>
                 {/* модуль Suspense призупиняє відображення компонента 
                     поки незакінчиться асинхронне завантаження React.lazy() */}
                 {/* в даному випадку покаже заглушку */}
                 <Suspense fallback={<div>Loading page...</div>}>
                     <Outlet />
                 </Suspense>
-            </div>
+            </Container>
         </>
     );
 }
