5255db357d1c463bf8d17d4ff6679121b14dff96
yuriisoroka165 <56719417+yuriisoroka165@users.noreply.github.com>
2023-03-21 15:44:06 +0000

    Deploying to gh-pages from @ yuriisoroka165/goit-react-hw-05-movies@8d602e5533b8e79ff2cbf0dc7e29c68a0e76aeda 🚀

---
+++ static/js/884.c743c82b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[884],{884:function(e,i,t){t.r(i);var s=t(184);i.default=function(){return(0,s.jsxs)("main",{children:[(0,s.jsx)("h1",{children:"Welcome Movies"}),(0,s.jsx)("p",{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas minima laudantium nesciunt ducimus excepturi sequi voluptatibus dicta? Molestias, culpa maxime."})]})}}}]);
+//# sourceMappingURL=884.c743c82b.chunk.js.map
