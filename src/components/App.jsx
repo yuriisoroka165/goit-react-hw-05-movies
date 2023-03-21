8d602e5533b8e79ff2cbf0dc7e29c68a0e76aeda
Yurii Soroka <yurisoroka165@gmail.com>
2023-03-21 17:43:09 +0200

    to home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,29 @@
 // import css from "./App.module.css";
 import { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import { SharedLayout } from "./SharedLayout";
+import { SharedLayout } from "./SharedLayout/SharedLayout";
 
-// React.lazy() - це метод який очікує функцію-завантажувач яка поаерне результат динамічного імпорту
+// React.lazy() - це метод який очікує функцію-завантажувач яка поdерне
+// результат динамічного імпорту
 // це проміс значенням якого буде дефолтний експорт модуля
 const Home = lazy(() => import("../pages/Home"));
 const Movies = lazy(() => import("../pages/Movies"));
+const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+const Cast = lazy(() => import("../components/Cast/Cast"));
+const Reviews = lazy(() => import("../components/Reviews/Reviews"));
 
 export function App() {
     return (
+        // опис всіх маршрутів
         <Routes>
             <Route path="/" element={<SharedLayout />}>
                 <Route index element={<Home />} />
-                <Route path="movies" element={<Movies />} />
+                {/* основний маршрут плюс вкладені сторінки даного маршрута */}
+                <Route path="movies" element={<Movies />}>
+                    <Route path="cast" element={<Cast />} />
+                    <Route path="reviews" element={<Reviews />} />
+                </Route>
+                <Route path="movies/:id" element={<MovieDetails />} />
             </Route>
         </Routes>
     );
